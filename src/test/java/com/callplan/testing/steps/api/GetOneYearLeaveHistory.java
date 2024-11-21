7e4a757ee2db2764f18c3b2ed415f5da94d5216d
Bonaventura Sanjaya <bonaventura.sanjaya@dexagroup.com>
2024-11-21 15:00:53 +0700

    feat: add test case get one year leave history

---
+++ src/test/java/com/callplan/testing/steps/api/GetOneYearLeaveHistory.java
@@ -0,0 +1,62 @@
+package com.callplan.testing.steps.api;
+
+import com.callplan.testing.steps.TestContext;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+
+public class GetOneYearLeaveHistory {
+  private final RequestSpecification request;
+
+  public GetOneYearLeaveHistory() {
+    this.request = TestContext.getRequest();
+  }
+
+  @When("I send POST request to \\/get-one-year-leave-history with payload:")
+  public void iSendPOSTRequestToGetOneYearLeaveHistoryWithPayload(String docString) throws JsonProcessingException {
+    Map<String, Object> jsonPayload = new ObjectMapper().readValue(docString, Map.class);
+    
+    Response response = request
+      .body(jsonPayload)
+      .post("/get-one-year-leave-history");
+        
+    TestContext.setResponse(response);
+  }
+
+  @And("the response should contain array of one year leave history data")
+  public void theResponseShouldContainArrayOfOneYearLeaveHistoryData() {
+    Response response = TestContext.getResponse();
+    response.then().body("data", hasSize(greaterThan(0)));
+  }
+
+  @And("each one year leave history data should match schema")
+  public void eachOneYearLeaveHistoryDataShouldMatchSchema() {
+    Response response = TestContext.getResponse();
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      String jsonArray = mapper.writeValueAsString(response.jsonPath().getList("data"));
+      assertThat(jsonArray, matchesJsonSchemaInClasspath("schemas/GetOneYearLeaveHistory.json"));
+    } catch (JsonProcessingException e) {
+      fail("Failed to parse response data: " + e.getMessage());
+    }
+  }
+
+  @And("the response should contain empty array of one year leave history data")
+  public void theResponseShouldContainEmptyArrayOfOneYearLeaveHistoryData() {
+    Response response = TestContext.getResponse();
+    response.then().body("data", hasSize(0));
+  }
+}
---
+++ src/test/resources/features/get-one-year-leave-history.feature
@@ -0,0 +1,43 @@
+Feature: Get One Year Leave History
+
+  @smoke
+  Scenario: Successfully get one year leave history
+    When I send POST request to /get-one-year-leave-history with payload:
+      """json
+      {
+        "personId" : 37697,
+        "rayonCode" : "TGRVIC0101"
+      }
+      """
+    Then the response status code should be 200
+    And the response should contain "success" with value "true"
+    And the response should contain array of one year leave history data
+    And each one year leave history data should match schema
+  
+  @negative
+  Scenario Outline: Get one year leave history with missing required field
+    When I send POST request to /get-one-year-leave-history with payload:
+      """
+        <payload>
+      """
+    Then the response status code should be 200
+    And the response should contain "success" with value "false"
+    And the response should contain error message
+    Examples:
+      | payload |
+      | {"personId" : 37697}         |
+      | {"rayonCode" : "TGRVIC0101"} |
+
+  @negative
+  Scenario Outline: Get one year leave history with invalid value
+    When I send POST request to /get-one-year-leave-history with payload:
+      """
+        <payload>
+      """
+    Then the response status code should be 200
+    And the response should contain "success" with value "true"
+    And the response should contain empty array of one year leave history data
+    Examples:
+      | payload |
+      | {"personId" : 0, "rayonCode" : "TGRVIC0101"} |
+      | {"personId" : 37697, "rayonCode" : ""} |
